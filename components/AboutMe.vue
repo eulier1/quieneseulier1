da30b756df206100308d9b7a29025dd5fec543dd
Eu <eulier1@gmail.com>
2020-11-19 10:28:20 -0300

    edit about description & add blog section

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -1,17 +1,7 @@
 <template>
-  <section
-    id="about"
-    class="overflow-hidden"
-  >
-    <v-row
-      class="white"
-      no-gutters
-    >
-      <v-col
-        class="pa-5"
-        cols="12"
-        md="6"
-      >
+  <section id="about" class="overflow-hidden">
+    <v-row class="white" no-gutters>
+      <v-col class="pa-5" cols="12" md="6">
         <base-bubble-1 style="transform: translate(5%, -5%)" />
 
         <base-heading class="info--text text-center">
@@ -19,28 +9,49 @@
         </base-heading>
 
         <base-text class="mb-10 pa-10">
-          A Venezuela-Born, Web Developer/Designer living in Buenos Aires.
-          <br>
-          I grew up in a bronce's craddle, living a fantastic, gaming bubble.
-          <br>
-          When all of that collapse thanks to the goverment, which responsability or at least in theory and according to Simon Bolivar, is to provide the greatest amount of happiness, social security & political stability possible.
-          <br>
-          But that doesn't happen, and reality bites. I constantly were in depression.
-          <br>
-          Why i'm talking all of this?, cause i don't wanna waste each other times with chit chat, faking glamarous presentionals and hollow, shallow, cable news interviews
-          <br>
-          You are looking for a person you can <strong>communicate</strong>, in a <strong>honest</strong>, <strong>transparent</strong> way while <strong>delivery-oriented results</strong> based on a budget.
-          <br>
-          Crisis is the most powerful motivator, each decisions you make need a purpose. So, since 4 years ago, i've been working creating web product using web technologies.
-          <br>
-          I'm interest in people which can colaborate with technologies.
+          A Venezuela-Born, Web Full Stack Developer living in Buenos Aires.
+          <br />
+          I migrate in 2019, now working location-independent.
+          <br />
+          Responsability & honest communication is what you need and get.
+          <br />
+          Facing a lot adversity taught me that.
+          <br />
+          Why i'm talking all of this?, cause i don't wanna waste each other
+          times with chit chat, faking glamarous presentionals and hollow,
+          shallow, cable news interviews.
+          <br />
+          Delivery-oriented results based on a budget.
+          <br />
+          4 years of experience, using web technologies.
+          <br />
         </base-text>
 
         <base-heading class="info--text text-center">
-          Languages & Tools
+          Blog
         </base-heading>
 
-        <v-alert
+        <v-container class="pa-5">
+          <v-row>
+            <v-col
+              cols="12"
+              md="12"
+              style="display:flex; justify-content:center; align-content: center; flex-wrap: wrap;"
+            >
+              <v-btn
+                color="primary mt-10"
+                large
+                @click="showAllProjects"
+                href="https://gallant-bell-850d88.netlify.app/2020/july/pic-in-pic-project.html"
+                target="_blank"
+              >
+                Take a look!
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+
+        <!-- <v-alert
 
           color="white"
           class="pa-12"
@@ -67,16 +78,17 @@
               height="8"
             />
           </v-row>
-        </v-alert>
+        </v-alert> -->
         <base-bubble-2 style="transform: translate(-190px, -5%)" />
       </v-col>
 
-      <v-col
-        class="hidden-sm-and-down"
-        md="6"
-      >
+      <v-col class="hidden-sm-and-down" md="6">
         <v-img
-          :src="$vuetify.breakpoint.mdAndDown ? 'https://i.imgur.com/s2nmCQz.jpg' : 'https://i.imgur.com/xq9bLUY.jpg'"
+          :src="
+            $vuetify.breakpoint.mdAndDown
+              ? 'https://i.imgur.com/s2nmCQz.jpg'
+              : 'https://i.imgur.com/xq9bLUY.jpg'
+          "
           height="100%"
         />
       </v-col>
@@ -85,39 +97,39 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      skills: [
-        {
-          name: 'Node.js',
-          value: 93,
-        },
-        {
-          name: 'Vue',
-          value: 90,
-        },
-        {
-          name: 'Nuxt',
-          value: 89,
-        },
+export default {
+  data: () => ({
+    skills: [
+      {
+        name: "Node.js",
+        value: 93,
+      },
+      {
+        name: "Vue",
+        value: 90,
+      },
+      {
+        name: "Nuxt",
+        value: 89,
+      },
 
-        {
-          name: 'Css/Sass',
-          value: 87,
-        },
-        {
-          name: 'Typescript',
-          value: 85,
-        },
-        {
-          name: 'Js',
-          value: 85,
-        },
-        {
-          name: 'Python',
-          value: 82,
-        },
-      ],
-    }),
-  }
+      {
+        name: "Css/Sass",
+        value: 87,
+      },
+      {
+        name: "Typescript",
+        value: 85,
+      },
+      {
+        name: "Js",
+        value: 85,
+      },
+      {
+        name: "Python",
+        value: 82,
+      },
+    ],
+  }),
+}
 </script>
